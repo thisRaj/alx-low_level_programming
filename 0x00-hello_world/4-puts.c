cece7a2c865cc660f5bd48c6fbd173922b0b98cf
thisRaj <muyekejalil@gmail.com>
2022-09-08 09:52:55 -0700

    again 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  * main - print the string in the put function
  *
  * Return: 0 Always (Succesful)
- */
+ * /
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
