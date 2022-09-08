fc84760b41c365a6f8b8553c36fabfebbdea5021
thisRaj <muyekejalil@gmail.com>
2022-09-08 14:23:47 -0700

    dg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - Entry point
  *
- * Description: using the main function
- * this program generates the assembly code
- * Return: 0
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-	
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long int: %ld byte(s)\n", sizeof(li);
-	printf("size of a long long int: %ld byte(s)\n", sizeof(lli);
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+char c;
+int i;
+long li;
+long long lli;
+float f;
+
+printf("size of a char: %ld byte(s)\n", sizeof(c));
+printf("size of an int: %ld byte(s)\n", sizeof(i));
+printf("size of a long int: %ld byte(s)\n", sizeof(li);
+printf("size of a long long int: %ld byte(s)\n", sizeof(lli);
+printf("size of a float: %ld byte(s)\n", sizeof(f));
+return (0);
 }
