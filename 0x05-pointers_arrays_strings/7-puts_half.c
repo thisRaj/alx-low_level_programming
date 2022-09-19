0b691e6ed606795fdce9b1d355e9be1b96d64937
thisRaj <muyekejalil@gmail.com>
2022-09-19 12:46:51 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,8 @@ void puts_half(char *str)
 {
 	int i, len = _strlen(str);
 
-	for (i = ((len - 1) / 2) + 1; i < len; i++)						putchar(*(str + i));
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
+		putchar(*(str + i));
 	putchar(10);
 }
 
