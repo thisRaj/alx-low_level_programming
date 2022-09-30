5d14e6953d56306a3fd2a932e68853ef453edee8
thisRaj <muyekejalil@gmail.com>
2022-09-30 08:40:02 -0700

    Task 0

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+* _strchr - a function that locates a character in a string
+*
+* @s: pointer to our string array input
+* @c: character to locate from input array
+*
+* Return: first occurence of charatcer or null if not found
+*/
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+s++;
+}
+if (*s == c)
+return (s);
+/*return null if not found*/
+return ('\0');
+}
